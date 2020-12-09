71bdd88047828568937902fd6720cc4b882c804a
mohonish <mohonish.c@gmail.com>
2020-12-08 20:04:33 -0800

    fixed correct naming and formatting of constraint description

--- src/components/ConstraintList.js
+++ src/components/ConstraintList.js
@@ -3,10 +3,28 @@ import { useSelector, useDispatch } from 'react-redux';
 import { ListGroup } from 'react-bootstrap';
 
 function ConstraintItem({ constraint }) {
-    var formattedStr = constraint.first.type + ' ' + constraint.first.id + ' ' + constraint.first.value
+    console.log('constraint: ', constraint)
+    var firstElement = useSelector(state => state.elements.byId[constraint.first.id])
+    var secondElement = useSelector(state => state.elements.byId[constraint.second.id])
+
+    var leftDescription = ''
+    if (constraint.first.type === 'element') {
+        leftDescription += firstElement.type + ' ' + firstElement.id
+    } else {
+        leftDescription += 'Page ' + firstElement.id
+    }
+
+    var rightDescription = ''
+    if (constraint.second.type === 'element') {
+        rightDescription += secondElement.type + ' ' + secondElement.id
+    } else {
+        rightDescription += 'Page ' + secondElement.id
+    }
+    
+    var formattedStr = leftDescription
     formattedStr += ' ' + constraint.relationship
-    formattedStr += ' ' + constraint.second.type + ' ' + constraint.second.id + ' ' + constraint.second.value
-    console.log('Constraint: ', formattedStr)
+    formattedStr += ' ' + rightDescription
+
     return <ListGroup.Item> 
         { formattedStr }
     </ListGroup.Item>
